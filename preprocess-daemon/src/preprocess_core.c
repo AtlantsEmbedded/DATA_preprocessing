ae68c40a99b6b48c6ebe9001b5aa11dbcf1335ea
fred <atom2626@hotmail.com>
2016-05-19 11:55:46 -0400

    Fine tuned the eye-blink detector

--- preprocess-daemon/src/preprocess_core.c
+++ preprocess-daemon/src/preprocess_core.c
@@ -20,7 +20,7 @@
 #define SIGNAL_THRESHOLD -90
 #define CHAN_1 0
 #define CHAN_4 3
-#define EYE_BLINK_LENGTH_IN_SECONDS 0.45
+#define EYE_BLINK_LENGTH_IN_SECONDS 0.5
 
 static int next_frame_has_blink = 0;
 static int eye_blink_length = 0;
